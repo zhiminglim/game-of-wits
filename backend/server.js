cf1d6c96c3d7cc6a7f7e751c13ba14530b7086bb
Zhiming <i-jovial@hotmail.com>
2020-12-28 16:20:00 +0800

    Add server.js and change scripting

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 //jshint esversion:6
 const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-
+const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 app.listen(5000, function() {
   console.log("Server started on port 5000.");
