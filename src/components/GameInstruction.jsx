62882d49cbb9ec1067f4646933f9f4da2dfebe36
Zhiming <i-jovial@hotmail.com>
2021-01-25 23:32:58 +0800

    Move history list now auto scrolls within a fixed height container

--- src/components/GameInstruction.jsx
+++ src/components/GameInstruction.jsx
@@ -16,6 +16,14 @@ function GameInstruction() {
             <p>O = Number is correct and in the correct position</p>
             <p>△ = Number is correct but in the wrong position</p>
             <p>X = None of the numbers nor the positions are correct</p>
+
+            <br/>
+            <p style={{ fontSize: "0.9rem" }}>
+              Example: if the hidden number is 123 and you guessed 139, 
+              the clue given will be 1O, 1△ because digit 1 is in the 
+              correct place and digit 2 is in the wrong place. 
+              X will only be given if all 3 digits are wrong.
+            </p>
           </div>
         </Col>
       </Row>
--- src/digits.js
+++ src/digits.js
@@ -2,47 +2,47 @@ const digitsList = [{
     id: 1,
     value: "1",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 2,
     value: "2",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 3,
     value: "3",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 4,
     value: "4",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 5,
     value: "5",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 6,
     value: "6",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 7,
     value: "7",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 8,
     value: "8",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }, {
     id: 9,
     value: "9",
     isDisabled: true,
-    variant: "secondary"
+    variant: "dark"
   }]
 
   export default digitsList;
