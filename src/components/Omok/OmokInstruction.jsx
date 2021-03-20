6aee706601725b7c1c2f8e2d90e3bdc432c7dc8d
Zhiming <i-jovial@hotmail.com>
2021-03-20 12:53:39 +0800

    Add mini AIR lab challenge

--- src/components/Omok/OmokInstruction.jsx
+++ src/components/Omok/OmokInstruction.jsx
@@ -4,7 +4,7 @@ function OmokInstruction() {
 
   return (
     <div className="game-intro-container">
-      <h3>Welcome to Omok! A true test of patience and cool.</h3>
+      <h3>Oops. You found something interesting...</h3>
     </div>
   );
 }
---
+++ src/components/Omok/WaypointChart.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CanvasJSReact from "../../assets/canvasjs.react";
+
+function WaypointChart(props) {
+  var CanvasJS = CanvasJSReact.CanvasJS;
+  var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+  const eachWaypoint = () => {
+    return props.wp.topWaypoints.map((waypoint) => {
+      return {
+        label: waypoint.name,
+        y: waypoint.count
+      };
+    });
+  };
+
+  const options = {
+    title: {
+      text: "Top Waypoints for " + props.wp.airport,
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: eachWaypoint(),
+      },
+    ],
+    axisY:{
+      interval: props.axisYInterval,
+      minimum: props.axisYMin
+    },
+  };
+
+
+  return (
+    <CanvasJSChart options={options} />
+  );
+}
+
+export default WaypointChart;
