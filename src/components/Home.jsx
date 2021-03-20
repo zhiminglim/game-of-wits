1d7cfd3a464a3ec300404dbb8e39228484ee2065
Zhiming <i-jovial@hotmail.com>
2021-03-20 13:58:34 +0800

    Add new game card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import numberImg from "../assets/images/game-card-img-3d.png";
+import goGame from "../assets/images/go-game.png";
 import { useHistory } from "react-router-dom";
+import { Card, CardDeck } from "react-bootstrap";
 
 function GameSelect() {
 
@@ -12,24 +13,53 @@ function GameSelect() {
     history.push("/game-3d");
   }
 
+  function playOmok() {
+    history.push("/game-omok");
+  }
+
   return (
     <div className="gameselect-container">
       <p className="intro-text">
-        Here to kill some time? Try some fun number games!
+        Here to kill some time? Try some fun mini-games!
       </p>
-      
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={numberImg} style={{padding: "0% 10%"}} />
-        <Card.Body>
-          <Card.Title>3D</Card.Title>
-          <Card.Text>
-            Braindance! Use the clues to guess a 3 digit number. Do you have
-            What It Takes?
-          </Card.Text>
-          <Button variant="dark" onClick={play3D}>Play</Button>
-        </Card.Body>
-      </Card>
 
+      <CardDeck>
+        <Card style={{ width: "14rem" }}>
+          <Card.Img
+            variant="top"
+            src={numberImg}
+            style={{ padding: "0% 10%" }}
+          />
+          <Card.Body>
+            <Card.Title>3D</Card.Title>
+            <Card.Text>
+              Braindance! Use the clues to guess a 3 digit number. Do you have
+              What It Takes?
+            </Card.Text>
+            <Button variant="dark" onClick={play3D}>
+              Play
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "14rem" }}>
+          <Card.Img
+            variant="top"
+            src={goGame}
+            style={{ padding: "0% 10%" }}
+          />
+          <Card.Body>
+            <Card.Title>Omok</Card.Title>
+            <Card.Text>
+              Connect 5! Literally connecting 5 pieces together. Do you have What It
+              Takes?
+            </Card.Text>
+            <Button variant="dark" onClick={playOmok}>
+              Play
+            </Button>
+          </Card.Body>
+        </Card>
+      </CardDeck>
     </div>
   );
 }
